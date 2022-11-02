7ff0b0bc935c5de38489263970780033c1940964
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-02 15:53:32 -0300

    Correção de bug

--- WEBCoding/ProjetoHTML-CSS/scripts/scriptgeneral.js
+++ WEBCoding/ProjetoHTML-CSS/scripts/scriptgeneral.js
@@ -1,17 +1,26 @@
+nav = document.getElementById('navigation')
+icon = document.getElementById('idicon')
+secondicon = document.getElementById('idicon2')
+
 function exibirnav() {
-    nav = document.getElementById('navigation')
-    icon = document.getElementById('idicon')
-    secondicon = document.getElementById('idicon2')
     nav.style.display="block"
     icon.style.display='none'
     secondicon.style.display='block'
 }
 
 function fecharnav() {
-    nav = document.getElementById('navigation')
-    icon = document.getElementById('idicon')
-    secondicon = document.getElementById('idicon2')
     nav.style.display='none'
     icon.style.display='block'
     secondicon.style.display='none'
+}
+
+function mudarTamanho(){ // Função utilizada para eliminar um pequeno bug (em caso de abertura e fechamento do menu mobile o menu desktop não era mostrado)
+    if(window.innerWidth>=780){
+        nav.style.display='block'
+        icon.style.display ='none'
+        secondicon.style.display='none'
+    } else{
+        nav.style.display='none'
+        icon.style.display ='block'
+    }
 }
