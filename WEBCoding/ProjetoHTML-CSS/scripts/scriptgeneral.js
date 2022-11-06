bc744eadb77a06f28273f13454389111d39590ac
Guilherme Freitas <guilhermefsantos9@gmail.com>
2022-11-06 20:47:51 -0300

    Comentando JS

--- WEBCoding/ProjetoHTML-CSS/scripts/scriptgeneral.js
+++ WEBCoding/ProjetoHTML-CSS/scripts/scriptgeneral.js
@@ -1,14 +1,16 @@
+/* Declaração de variáveis necessárias para funções */
+
 nav = document.getElementById('navigation')
 icon = document.getElementById('idicon')
 secondicon = document.getElementById('idicon2')
 
-function exibirnav() {
+function exibirnav() { // Função utilizada para exibição do menu nav
     nav.style.display="block"
     icon.style.display='none'
     secondicon.style.display='block'
 }
 
-function fecharnav() {
+function fecharnav() { // Função utilizada para o fechamento do menu nav
     nav.style.display='none'
     icon.style.display='block'
     secondicon.style.display='none'
