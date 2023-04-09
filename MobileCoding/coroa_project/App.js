507103e883e81ab44a0632f0f2390e26cc4ca118
Guilherme Freitas <guilhermefsantos9@gmail.com>
2023-04-09 13:58:48 -0300

    add icons

--- MobileCoding/coroa_project/App.js
+++ MobileCoding/coroa_project/App.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Text, View, StyleSheet,Image } from 'react-native';
+import { Text, View, StyleSheet,Image, TextInput, Touchable, TouchableOpacity } from 'react-native';
+import {FontAwesome} from '@expo/vector-icons';
 import Constants from 'expo-constants';
 
 
@@ -8,35 +9,108 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.containerBox}>
-        <Image style={styles.imgPrin} />
+        <Image style={styles.imgPrin} source={require('./assets/COROA-nofill.png')} />
         <Text style={styles.paragraph}>
           C.O.R.O.A
         </Text>
+        <Text style={styles.subparag}>O aplicativo que cuida de você!</Text>
       </View>
+      <View style={styles.containerInput}>
+        <View style={styles.inputcpf}>
+          <FontAwesome name='user' size={20} color="#505050"></FontAwesome>
+          <TextInput style={styles.cpfTextInp} placeholder='Digite seu CPF'></TextInput>
+        </View>
+        <View style={styles.inputpassw}>
+          <FontAwesome name='lock' size={20} color="#505050"></FontAwesome>
+          <TextInput style={styles.passTextInp} placeholder='Digite sua senha'></TextInput>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.logButton}>
+        <Text style={styles.textButton}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#ecf0f1',
-    padding: 20,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  containerBox: {
-    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: '#FFC9AB',
     alignItems: 'center',
   },
+  containerBox: {
+    marginTop: 200,
+    width: 500,
+    height: 130,
+    backgroundColor: '#FFC9AB',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  containerInput: {
+    marginTop: 100,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
   paragraph: {
-    marginLeft: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
+    fontSize: 45,
+    fontWeight: '500',
+    fontFamily: 'serif',
+    color: '#911746'
+  },
+  subparag: {
+    fontSize: 11,
+    fontWeight: '400',
     fontFamily: 'serif',
     color: '#911746'
   },
   imgPrin: {
-    width: 100,
-    height: 100
+    width: 150,
+    height: 150
+  },
+  inputcpf: {
+    flexDirection: 'row',
+    borderRadius: 5,
+    padding: 13,
+    backgroundColor: '#f0f0f0',
+    width: 290,
+    height: 50,
+    borderWidth:1,
+    borderColor: '#777'
+  },
+  inputpassw: {
+    flexDirection: 'row',
+    marginTop: 20,
+    borderRadius: 5,
+    padding: 14,
+    backgroundColor: '#f0f0f0',
+    width: 290,
+    height: 50,
+    borderWidth:1,
+    borderColor: '#777'
+  },
+  cpfTextInp: {
+    marginLeft: 10,
+    fontSize: 17,
+  },
+  passTextInp: {
+    marginLeft: 10,
+    fontSize: 17,
+  },
+  logButton: {
+    borderRadius: 7,
+    display: 'flex',
+    marginTop: 30,
+    backgroundColor: '#741B47',
+    width: '40%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textButton:{
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fafafa',
+    textAlign: 'center',
   },
 });
