99acb721f2d3c6579f1b66841d0dcdefd08cabf8
Guilherme Freitas <99972010+guifreitasds@users.noreply.github.com>
2023-10-19 09:27:58 -0300

    Add files via upload

---
+++ DataBase/BD_Coroa.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `caregiver` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `cpf` varchar(11),
+  `email` varchar(255),
+  `password` varchar(255),
+  `birth_date` date,
+  `coren` varchar(255),
+  `crm` varchar(255),
+  `created_at` timestamp
+);
+
+CREATE TABLE `patient` (
+  `id` integer PRIMARY KEY,
+  `username` varchar(255),
+  `cpf` varchar(11),
+  `email` varchar(255),
+  `password` varchar(255),
+  `birth_date` date,
+  `caregiver_id` integer,
+  `created_at` timestamp
+);
+
+CREATE TABLE `medicines` (
+  `id` integer PRIMARY KEY,
+  `title` varchar(255),
+  `body` text COMMENT 'Medicine description',
+  `patient_id` integer,
+  `status` varchar(255),
+  `created_at` timestamp
+);
+
+ALTER TABLE `medicines` ADD FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`);
+
+ALTER TABLE `patient` ADD FOREIGN KEY (`caregiver_id`) REFERENCES `caregiver` (`id`);
