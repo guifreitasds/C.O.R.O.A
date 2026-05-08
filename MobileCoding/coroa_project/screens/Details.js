fc48a193d7f1ed6e4eff40e28697c53b4b4dc164
Guilherme <guilhermefsantos9@gmail.com>
2026-05-08 14:42:37 -0300

    merge commit

---
+++ MobileCoding/coroa_project/screens/Details.js
@@ -0,0 +1,77 @@
+import { Text, View, ScrollView, TextInput, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
+
+export function DetailsScreen({ navigation, props, route }) {
+
+    const data = route.params.data;
+
+    const deleteData = (data) => {
+        fetch(`http://192.168.31.198:5000/delete/${data.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(data => {
+            navigation.navigate('Medicines')
+        })
+        .catch(error => console.log(error))
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.Main}>
+                <Text style={{ fontSize: 25 }}>
+                    {data.title}
+                </Text >
+
+                <Text
+                    style={{ fontSize: 20, marginTop: 10 }}>
+                    Descrição: {data.description}
+                </Text>
+            </View>
+            <View style={styles.buttons}>
+                <Button
+                    style={{ margin: 10 }}
+                    title='Editar'
+                    mode='contained'
+                    onPress={() => navigation.navigate("Editar", {data: data})}
+                ></Button>
+
+                <Button
+                    style={{ margin: 10 }}
+                    title='Excluir'
+                    mode='contained'
+                    onPress={() => deleteData(data)}
+                ></Button>
+
+
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        textAlign: 'center',
+        backgroundColor: '#FFC9AB',
+        display: 'flex',
+        alignItems: 'center',
+
+    },
+    Main: {
+        backgroundColor: "#ffffff",
+        borderRadius: 5,
+        margin: 10,
+        padding: 10
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: "space-around",
+        margin: 15,
+        padding: 10,
+        gap: 20
+    },
+})
---
+++ MobileCoding/coroa_project/screens/Edit.js
@@ -0,0 +1,73 @@
+import { Text, View, ScrollView, TextInput, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
+
+export function EditScreen({ navigation, props, route }) {
+
+
+    const data = route.params.data
+    const [title, setTitle] = useState(data.title)
+    const [description, setDesc] = useState(data.description)
+
+
+    const updateData = () => {
+        fetch(`http://192.168.31.198:5000/update/${data.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title: title, description: description })
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                navigation.navigate('Medicines', {data:data})
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Adicione seu remédio</Text>
+            <TextInput style={styles.inputTitle}
+                label="Titulo"
+                value={title}
+                mode="outlined"
+                onChangeText={text => setTitle(text)}
+            />
+            <TextInput style={styles.inputTitle}
+                label="Descrição"
+                value={description}
+                mode="outlined"
+                multiline
+                numberOfLines={10}
+                onChangeText={desc => setDesc(desc)}
+            />
+            <Button
+                style={{ margin: 10 }}
+                title='Editar'
+                mode='contained'
+                onPress={() => updateData()}
+            ></Button>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        textAlign: 'center',
+        backgroundColor: '#FFC9AB',
+        display: 'flex',
+        alignItems: 'center',
+
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: "bold"
+    },
+    inputTitle: {
+        backgroundColor: '#f0f0f0',
+        width: 300,
+        height: 50,
+        margin: 20,
+    }
+})
